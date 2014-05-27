aeb1ed3cd6574de466364d118c61eacc6f4e10c0
ZeToken <github@zetoken.net>
2014-05-27 12:22:10 +0200

    - Project Structure Cleanup

--- WSCT.GUI.Launcher/Program.cs
+++ WSCT.GUI.Launcher/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Windows.Forms;
 
 namespace WSCT.GUI.Launcher
@@ -15,7 +13,7 @@ namespace WSCT.GUI.Launcher
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new WSCT.GUI.WinSCardGUI());
+            Application.Run(new WinSCardGui());
 
             // Force close all other existing Application.Run() 
             Application.Exit();
